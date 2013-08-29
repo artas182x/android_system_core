309054f0985b8c9f1058bfeb900d9799bc3db2e1
MWisBest <repinski23@gmail.com>
2013-08-28 20:22:14 -0500

    ADB: Don't force -O2 if something else is provided instead.

--- adb/Android.mk
+++ adb/Android.mk
@@ -74,7 +74,14 @@ else
   LOCAL_SRC_FILES += fdevent.c
 endif
 
-LOCAL_CFLAGS += -O2 -g -DADB_HOST=1  -Wall -Wno-unused-parameter
+LOCAL_CFLAGS += -g -DADB_HOST=1  -Wall -Wno-unused-parameter
+# ADB requires optimizations to build, so if none are provided, force it.
+ifeq ($(findstring -O, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
+ifneq ($(findstring -O0, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 LOCAL_MODULE := adb
 LOCAL_MODULE_TAGS := debug
@@ -116,7 +123,14 @@ LOCAL_SRC_FILES := \
 	remount_service.c \
 	usb_linux_client.c
 
-LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wall -Wno-unused-parameter
+LOCAL_CFLAGS := -g -DADB_HOST=0 -Wall -Wno-unused-parameter
+# ADB requires optimizations to build, so if none are provided, force it.
+ifeq ($(findstring -O, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
+ifneq ($(findstring -O0, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
@@ -158,7 +172,6 @@ LOCAL_SRC_FILES := \
 	fdevent.c
 
 LOCAL_CFLAGS := \
-	-O2 \
 	-g \
 	-DADB_HOST=1 \
 	-DADB_HOST_ON_TARGET=1 \
@@ -168,6 +181,14 @@ LOCAL_CFLAGS := \
 	-D_GNU_SOURCE
 
 LOCAL_C_INCLUDES += external/openssl/include
+
+# ADB requires optimizations to build, so if none are provided, force it.
+ifeq ($(findstring -O, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
+ifneq ($(findstring -O0, $(HOST_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
 
 LOCAL_MODULE := adb
 
